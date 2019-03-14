e779af7491c3127b54e4d366366a98251027f112
Mextrin <seba3552@hotmail.com>
2019-03-15 00:59:30 +0100

    Multi-Controller support (still WIP)
    Changed input bindings and added not yet fully working multi-controller support

---
+++ Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInput : MonoBehaviour
+{
+    static List<int> assignedControllers = new List<int>();
+    int controllerID = 1;
+
+    public bool ConnectedController { get { return controllerID > 0; } }
+    public string Horizontal { get { return "Controller" + controllerID + "Horizontal"; } }
+    public string Vertical { get { return "Controller" + controllerID + "Vertical"; } }
+    public string ButtonA { get { return "Controller" + controllerID + "A"; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controllerID <= 0)
+        {
+            //See if any controller activates
+            for (int i = 0; i < 2; i++)
+            {
+                if (Input.GetButtonDown("Controller" + (i + 1) + "A"))
+                    controllerID = i + 1;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/LocalPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ControllerInput))]
+public class LocalPlayer : MonoBehaviour
+{
+    ControllerInput input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = GetComponent<ControllerInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (input.ConnectedController)
+        {
+
+        }
+        print(Input.GetAxis(input.ButtonA));
+    }
+}
