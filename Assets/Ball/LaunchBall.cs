9885ef11711ae29c1be71c91fb0e93f38016a1bb
Mextrin <seba3552@hotmail.com>
2019-03-15 16:12:10 +0100

    Commented out Launchball.cs
    Due to the massive amount of errors in almost every variable call, especially for private variables, I am commenting out to not cause an error spam.

--- Assets/Ball/LaunchBall.cs
+++ Assets/Ball/LaunchBall.cs
@@ -4,93 +4,93 @@ using UnityEngine;
 
 public class LaunchBall : MonoBehaviour
 {
-    #region Variabels
-    [HideInInspector]
-    public bool CanFire;
-
-    [SerializeField]
-    private float LaunchForce;
-    [SerializeField]
-    private float time;
-
-    Rigidbody2D BallRb2D;
-
-    PlayerLaunchAction PlayerLaAction;
-    HitBehaviour EnHitBehaviour;
-    #endregion Variabels
-
-
-    private void Awake()
-    {
-        BallRb2D = GetComponent<Rigidbody2D>();
-        PlayerLaAction = GameObject.FindObjectOfType<PlayerLaunchAction>()?.GetComponent<PlayerLaunchAction>();
-        EnHitBehaviour = GameObject.FindObjectOfType<HitBehaviour>()?.GetComponent<HitBehaviour>();
-    }
-
-    private void Update()
-    {
-        Launch();   
-    }
-
-    void Launch()
-    {
-        Vector2 vel = (transform.position - PlayerLaAction.transform.position) * LaunchForce;
-        time = 10;
-
-        if (PlayerLaAction != null)
-        {
-            if (PlayerLaAction.hasLaunched == true && CanFire == true)
-            {
-                /*Debug.Log("LAUNCH!");*/
-                BallRb2D.gravityScale = 1;
-                BallRb2D.bodyType = RigidbodyType2D.Dynamic;
-                BallRb2D.velocity += vel;
-
-               /* Invoke("BallVelDecreasment", time);*/
-            }
-        }
-        else
-        {
-            PlayerLaAction = null;
-        }
-    }
-
-    void BallVelDecreasment()
-    {
-//         PhysMat.friction = 5;
-//         PhysMat.bounciness = 0;
-        /*Debug.Log("Slowed down");*/
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject == PlayerLaAction.gameObject)
-        {
-            CanFire = true;
-            /*Debug.Log("Player detected");*/
-        }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject == PlayerLaAction.gameObject)
-        {
-            CanFire = false;
-            /*Debug.Log("Player NOT detected");*/
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (EnHitBehaviour != null && (collision.gameObject == EnHitBehaviour.gameObject))
-        {
-            /*Debug.Log("Enemy Hit");*/
-            EnHitBehaviour.isHit = true;
-            EnHitBehaviour.timeDown = EnHitBehaviour.resetTime;
-        }
-    }
-
-    //     private void OnGUI()
-    //     {
-    //         GUI.Label(new Rect(5f, 5f, 200f, 200f), ("Velocity: " + BallRb2D.velocity.ToString()));
-    //     }
+    //#region Variabels
+    //[HideInInspector]
+    //public bool CanFire;
+    //
+    //[SerializeField]
+    //private float LaunchForce;
+    //[SerializeField]
+    //private float time;
+    //
+    //Rigidbody2D BallRb2D;
+    //
+    //PlayerLaunchAction PlayerLaAction;
+    //HitBehaviour EnHitBehaviour;
+    //#endregion Variabels
+    //
+    //
+    //private void Awake()
+    //{
+    //    BallRb2D = GetComponent<Rigidbody2D>();
+    //    PlayerLaAction = GameObject.FindObjectOfType<PlayerLaunchAction>()?.GetComponent<PlayerLaunchAction>();
+    //    EnHitBehaviour = GameObject.FindObjectOfType<HitBehaviour>()?.GetComponent<HitBehaviour>();
+    //}
+    //
+    //private void Update()
+    //{
+    //    Launch();   
+    //}
+    //
+    //void Launch()
+    //{
+    //    Vector2 vel = (transform.position - PlayerLaAction.transform.position) * LaunchForce;
+    //    time = 10;
+    //
+    //    if (PlayerLaAction != null)
+    //    {
+    //        if (PlayerLaAction.hasLaunched == true && CanFire == true)
+    //        {
+    //            /*Debug.Log("LAUNCH!");*/
+    //            BallRb2D.gravityScale = 1;
+    //            BallRb2D.bodyType = RigidbodyType2D.Dynamic;
+    //            BallRb2D.velocity += vel;
+    //
+    //           /* Invoke("BallVelDecreasment", time);*/
+    //        }
+    //    }
+    //    else
+    //    {
+    //        PlayerLaAction = null;
+    //    }
+    //}
+    //
+    //void BallVelDecreasment()
+    //{
+//  //       PhysMat.friction = 5;
+//  //       PhysMat.bounciness = 0;
+    //    /*Debug.Log("Slowed down");*/
+    //}
+    //
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject == PlayerLaAction.gameObject)
+    //    {
+    //        CanFire = true;
+    //        /*Debug.Log("Player detected");*/
+    //    }
+    //}
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject == PlayerLaAction.gameObject)
+    //    {
+    //        CanFire = false;
+    //        /*Debug.Log("Player NOT detected");*/
+    //    }
+    //}
+    //
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (EnHitBehaviour != null && (collision.gameObject == EnHitBehaviour.gameObject))
+    //    {
+    //        /*Debug.Log("Enemy Hit");*/
+    //        EnHitBehaviour.isHit = true;
+    //        EnHitBehaviour.timeDown = EnHitBehaviour.resetTime;
+    //    }
+    //}
+    //
+    ////     private void OnGUI()
+    ////     {
+    ////         GUI.Label(new Rect(5f, 5f, 200f, 200f), ("Velocity: " + BallRb2D.velocity.ToString()));
+    ////     }
 }
