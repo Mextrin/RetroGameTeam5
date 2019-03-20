3be3731f1797282177f9ad7108dd9f13cdb4f3ae
Mextrin <seba3552@hotmail.com>
2019-03-20 09:49:43 +0100

    Added a GameManager script
    Added a script to control the state of the game as well as a prefab that only requires dragging out into a scene to start working.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    List<LocalPlayer> players = new List<LocalPlayer>();
+    // Start is called before the first frame update
+    void Awake()
+    {
+        var playerObjects = FindObjectsOfType<LocalPlayer>();
+        for (int i = 0; i < playerObjects.Length; i++)
+        {
+            players.Add(playerObjects[i].GetComponent<LocalPlayer>());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (players.Count > 0)
+        {   
+            //Any player alive
+            for (int i = 0; i < players.Count; i++)
+            {
+                HealthComponent playerHealth = players[i].GetComponent<HealthComponent>();
+
+                if (playerHealth.CurrentHealth <= 0)
+                {
+                    players.RemoveAt(i);
+                }
+            }
+        }
+        else
+        {
+            //Both players dead
+        }
+    }
+}
