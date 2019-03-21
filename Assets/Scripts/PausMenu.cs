e918b35e67660deee9db84ca4ed374650ba297f5
MarcoTur <marko.turzynski@futuregames.nu>
2019-03-21 10:34:23 +0100

    working on the pause menu

---
+++ Assets/Scripts/PausMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Pause)) ;
+        {
+            Debug.Log("PAUSED");
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paus();
+            }
+        }
+    }
+
+    void Paus()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
